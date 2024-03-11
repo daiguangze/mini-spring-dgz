1c11264b0713d775635b09b66a4d720dff4573f5
daiguangze <daishuaishuai.z@qq.com>
2024-03-11 14:40:56 +0800

    feat  simple bean factory

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # mini-spring-dgz
 mini spring 
+
+
+# IOC
+
+
+## 最简单的bean容器
+```java
+public class BeanFactory {
+	private Map<String, Object> beanMap = new HashMap<>();
+
+	public void registerBean(String name, Object bean) {
+		beanMap.put(name, bean);
+	}
+
+	public Object getBean(String name) {
+		return beanMap.get(name);
+	}
+}
+```
+
---
+++ src/test/java/org/springframework/test/ioc/SimpleBeanContainerTest.java
@@ -0,0 +1,26 @@
+package org.springframework.test.ioc;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleBeanContainerTest {
+    @Test
+    public void testGetBean() throws Exception{
+        BeanFactory beanFactory = new BeanFactory();
+        beanFactory.registerBean("hello",new HelloService());
+        HelloService service = (HelloService) beanFactory.getBean("hello");
+        assertThat(service).isNotNull();
+        assertThat(service.sayHello()).isEqualTo("hello");
+    }
+
+
+    class HelloService{
+        public String sayHello(){
+            System.out.println("hello");
+            return "hello";
+        }
+
+    }
+}
